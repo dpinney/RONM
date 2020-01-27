52b13d43417d164bd39faa35ebc4611e89a2cad0
David Pinney <david@pinney.org>
2020-01-27 18:21:03 -0500

    oops, forgot tar

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
 # run each of the (.dss, .json) input pairs through ONM and other tools
-sec_circ = open('test_data/test98_circuit_secret.dss').read()
+
+import os
+os.system('tar xvf other_test_data.tar')
+sec_circ = open('secret_test_data/test98_circuit_secretCoop.dss').read()
 print(sec_circ)
